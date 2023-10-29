8d1322d68893cb46961e2f5a456905de5c1fc204
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-29 00:39:22 -0300

    Adicionando user

---
+++ FeiraConnect/Model/User.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FeiraConnect.Model
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Senha { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(15)]
+        public string Telefone { get; set; } = string.Empty;
+
+    }
+}

ca95a6bf31c675c4c8f798b5d3ca0dd2773e2d4d
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-29 00:57:58 -0300

    Chave de assinatura do Token JWT

---
+++ FeiraConnect/Security/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace FeiraConnect.Security
+{
+    public class Settings
+    {
+        private static string secret = "6775a44458cc015db1d788e47c433b8bd4b7bde1c0038462d69f9cdddf87f563";
+
+        public static string Secret { get => secret; set => secret = value; }
+    }
+}

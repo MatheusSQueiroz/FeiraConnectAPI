1fc85384e0330dd6f80617a59f323a2b247887b4
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-29 00:40:51 -0300

    Adicionando userlogin

---
+++ FeiraConnect/Model/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿namespace FeiraConnect.Model
+{
+    public class UserLogin
+    {
+        public long Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Senha { get; set; } = string.Empty;
+
+        public string Telefone { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+    }
+}

0d7715ed04a1e8ba8e436902dff37a0d1fe9b811
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-29 01:04:43 -0300

    Implementando validação do token

--- FeiraConnect/Program.cs
+++ FeiraConnect/Program.cs
@@ -1,11 +1,16 @@
 
+using blogpessoal.Security.Implements;
 using FeiraConnect.Data;
 using FeiraConnect.Model;
+using FeiraConnect.Security;
 using FeiraConnect.Service;
 using FeiraConnect.Service.Implements;
 using FeiraConnect.Validator;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace FeiraConnect
 {
@@ -31,6 +36,26 @@ namespace FeiraConnect
 
             builder.Services.AddScoped<IFeiraService, FeiraService>();
             builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddTransient<IAuthService, AuthService>();
+
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var Key = Encoding.UTF8.GetBytes(Settings.Secret);
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Key)
+                };
+            });
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();

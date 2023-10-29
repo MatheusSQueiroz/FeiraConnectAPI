6e7a9a9f282f235c621d17f6c93d82ef4e08e4ac
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-28 23:58:13 -0300

    Model da feira

---
+++ FeiraConnect/Model/Feira.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using FeiraConnect.Util;
+using Newtonsoft.Json;
+
+namespace FeiraConnect.Model
+{
+    public class Feira
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(255)]
+        public string Local { get; set; } = string.Empty;
+
+        [Column(TypeName = "date")]
+        [JsonConverter(typeof(DateOnlyJsonConverter))]
+        public DateTime DataLancamento { get; set; }
+
+    }
+}
